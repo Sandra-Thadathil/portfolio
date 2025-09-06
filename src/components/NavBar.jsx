c7c978caecdaaa3d5ed2f6961245df6bc444b7ad
Sandra S <sthadathil03@gmail.com>
2025-09-06 01:23:42 -0300

    Minor changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar() {
     <nav>
       <Link to="/" style={{ margin: '0 1rem' }}>Home</Link>
       <Link to="/work" style={{ margin: '0 1rem' }}>Work</Link>
-      <Link to="/project" style={{ margin: '0 1rem' }}>Project</Link>
+      <Link to="/project" style={{ margin: '0 1rem' }}>Projects</Link>
     </nav>
   );
 }
