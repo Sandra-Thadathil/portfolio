be657642ed85e6ba0f82a621cb5c2865811171e5
Sandra S <sthadathil03@gmail.com>
2025-09-10 22:38:16 -0300

    Add menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css'; 
 
 export default function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav>
-      <Link to="/" style={{ margin: '0 1rem' }}>Home</Link>
-      <Link to="/work" style={{ margin: '0 1rem' }}>Work</Link>
-      <Link to="/project" style={{ margin: '0 1rem' }}>Projects</Link>
-      <Link to="/contact" style={{ margin: '0 1rem' }}>Contact</Link>
+    <nav className="navbar">
+      <button className="hamburger" onClick={toggleMenu}>
+        ☰
+      </button>
+      <div className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+        <Link to="/" onClick={() => setIsMenuOpen(false)} style={{ margin: '0 1rem' }}>Home</Link>
+        <Link to="/work" onClick={() => setIsMenuOpen(false)} style={{ margin: '0 1rem' }}>Work</Link>
+        <Link to="/project" onClick={() => setIsMenuOpen(false)} style={{ margin: '0 1rem' }}>Projects</Link>
+        <Link to="/contact" onClick={() => setIsMenuOpen(false)} style={{ margin: '0 1rem' }}>Contact</Link>
+      </div>
     </nav>
   );
 }
